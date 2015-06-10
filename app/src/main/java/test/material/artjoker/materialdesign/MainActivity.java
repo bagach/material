c98423f752b0801db7c491a73f8227ea70e9cbff
Alexandr Bagach <bagach.alexandr@gmail.com>
2015-06-10 14:17:35 +0300

    create test app for material design in android phones with api.version < L

--- app/src/main/java/test/material/artjoker/materialdesign/MainActivity.java
+++ app/src/main/java/test/material/artjoker/materialdesign/MainActivity.java
@@ -47,7 +47,6 @@ public class MainActivity extends ActionBarActivity {
 
     private void initializeDrawer() {
         AccountHeader header = createAccountHeader();
-
         drawer = new DrawerBuilder()
                 .withActivity(this)
                 .withToolbar(toolbar)
